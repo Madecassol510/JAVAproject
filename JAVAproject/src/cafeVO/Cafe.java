bd2acc00092b9e13161a19b64d4937978b5591ca
jiyong <boradory1346@naver.com>
2023-09-13 17:51:47 +0900

    2023/09/13

--- JAVAproject/src/cafeVO/Cafe.java
+++ JAVAproject/src/cafeVO/Cafe.java
@@ -4,18 +4,22 @@ public class Cafe {
 	
 	
 	String name;
+	String adress;
 	
 	Coffee[] coffee;
 	Ade[] ade;
 	Smoothie[] smoothie;
 	Tea[] tea;
 	
-	public Cafe(String name, Coffee[] coffee, Ade[] ade, Smoothie[] smoothie, Tea[] tea ) {
+	public Cafe(String name,String adress, Coffee[] coffee, Ade[] ade, Smoothie[] smoothie, Tea[] tea ) {
 		this.name = name;
+		this.adress = adress;
+		
 		this.coffee = coffee;
 		this.ade = ade;
 		this.smoothie = smoothie;
 		this.tea = tea;
+		
 		
 	}
 }

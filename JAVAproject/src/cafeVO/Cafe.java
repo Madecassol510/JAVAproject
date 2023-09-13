4f935291808c150f849fe8a0483f70d0c99d4eb1
jiyong <boradory1346@naver.com>
2023-09-13 17:56:20 +0900

    Cafe클래스 get/set 추가

--- JAVAproject/src/cafeVO/Cafe.java
+++ JAVAproject/src/cafeVO/Cafe.java
@@ -19,7 +19,53 @@ public class Cafe {
 		this.ade = ade;
 		this.smoothie = smoothie;
 		this.tea = tea;
-		
-		
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAdress() {
+		return adress;
+	}
+
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+
+	public Coffee[] getCoffee() {
+		return coffee;
+	}
+
+	public void setCoffee(Coffee[] coffee) {
+		this.coffee = coffee;
+	}
+
+	public Ade[] getAde() {
+		return ade;
+	}
+
+	public void setAde(Ade[] ade) {
+		this.ade = ade;
+	}
+
+	public Smoothie[] getSmoothie() {
+		return smoothie;
+	}
+
+	public void setSmoothie(Smoothie[] smoothie) {
+		this.smoothie = smoothie;
+	}
+
+	public Tea[] getTea() {
+		return tea;
+	}
+
+	public void setTea(Tea[] tea) {
+		this.tea = tea;
 	}
 }
